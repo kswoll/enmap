ec95b6e172713404ad94e9994aa723e5871c1b90
Kirk Woll <kirk.woll@gmail.com>
2015-01-06 16:12:47 -0800

    Add ToDictionaryAsync extensions

--- Enmap/TaskEnumerableAsyncExtensions.cs
+++ Enmap/TaskEnumerableAsyncExtensions.cs
@@ -7,6 +7,20 @@ namespace Enmap
 {
     public static class TaskEnumerableAsyncExtensions 
     {
+        public static async Task<Dictionary<TKey, T>> ToDictionaryAsync<T, TKey>(this Task<IEnumerable<T>> source,
+            Func<T, TKey> keySelector)
+        {
+            var result = await source;
+            return result.ToDictionary(keySelector);
+        }
+
+        public static async Task<Dictionary<TKey, TValue>> ToDictionaryAsync<T, TKey, TValue>(this Task<IEnumerable<T>> source,
+            Func<T, TKey> keySelector, Func<T, TValue> valueSelector)
+        {
+            var result = await source;
+            return result.ToDictionary(keySelector, valueSelector);
+        }
+
         public static async Task<T[]> ToArrayAsync<T>(this Task<IEnumerable<T>> source)
         {
             var result = await source;
