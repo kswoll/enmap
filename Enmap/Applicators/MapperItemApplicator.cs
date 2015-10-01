3be399435aff38e4a8f63b2b5b18a360dc29921a
Kirk Woll <kirk.woll@gmail.com>
2015-10-01 07:10:47 -0700

    Fix error message

--- Enmap/Applicators/MapperItemApplicator.cs
+++ Enmap/Applicators/MapperItemApplicator.cs
@@ -53,7 +53,7 @@ namespace Enmap.Applicators
             }
             catch (Exception e)
             {
-                throw new Exception(string.Format("Error assigning '{0}.{1}' of type {2} to destination '{3}' of type {3}",
+                throw new Exception(string.Format("Error assigning '{0}.{1}' of type {2} to destination '{3}' of type {4}",
                     Item.From.GetPropertyInfo().DeclaringType, Item.Name, transientValue == null ? "null" : transientValue.GetType().FullName, Item.For.GetPropertyName(), Item.For.GetPropertyInfo().PropertyType.FullName), e);
             }
         }
